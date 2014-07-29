e9da993e2f818cfc63d88c4ba5d97dd63ccbf9b6
Élie Michel <elie.michel@ens.fr>
2014-07-29 20:39:41 +0200

    Passage à SQLite
    Changement de couleur des messages.
    Ajout du nom du mec qui doit bouger.
    Stats relatives au nombre de messages (pas sûr que ça soit au point).

--- nsfbot.py
+++ nsfbot.py
@@ -6,25 +6,26 @@ import sys
 import time
 import re
 import thread
+import sqlite3
 
 debug = True
 
-words = {
-	'#courssysteme': {},
-	'#b6': {}
-}
+chans = ['#courssysteme', '#b6']
+count = {c: 0 for c in chans}
 
 class IRCCat(ircbot.SimpleIRCClient):
-	def __init__(self, targets):
+	def __init__(self, targets, db):
 		ircbot.SimpleIRCClient.__init__(self)
 		self.targets = targets
 		self.server = None
+		self.db = db
+		self.cur = db.cursor()
 	
 	def sayto(self, target, msg):
 		if self.server == None:
 			print("Target not joined yet : unable to say \"%s\"" % (msg,))
 		else:
-			self.server.privmsg(target, "%c14%s" % (3, msg))
+			self.server.privmsg(target, "%c34%s" % (3, msg))
 
 	def on_welcome(self, server, ev):
 		print("Welcomed")
@@ -38,7 +39,6 @@ class IRCCat(ircbot.SimpleIRCClient):
 
 	def on_disconnect(self, server, ev):
 		sys.exit(0)
-		self.say('hello')
 	
 	def on_pubmsg(self, server, ev):
 		chan = ev.target()
@@ -46,19 +46,23 @@ class IRCCat(ircbot.SimpleIRCClient):
 		msg = msg_.lower()
 		nick = ev.source().split('!')[0]
 
-		score = {c: 0 for c in words.keys()}
-		for w in msg.split(' '):
-			if not w in words[chan]:
-				words[chan][w] = 0
-			words[chan][w] += 1
+		words = re.split('\W+', msg)
+		count[chan] += 1
+		entries = []
+		for w in words:
+			entries.extend([{'word': w, 'chan': c, 'incr': 1 if c == chan else 0} for c in chans])
+		q = "INSERT OR REPLACE INTO Word (word, chan, count) VALUES (:word, :chan, COALESCE((SELECT count + :incr FROM Word WHERE word = :word AND chan = :chan), 0))"
+		self.cur.executemany(q, entries)
+		self.db.commit()
 
-			for c in score.keys():
-				if w in words[c]:
-					score[c] += words[c][w]
+		q = "SELECT chan, SUM(count) FROM Word WHERE word IN (%s) GROUP BY chan" % (', '.join(['?'] * len(words)),)
+		self.cur.execute(q, words)
+		score = {s[0]: float(s[1]) / count[s[0]] for s in self.cur.fetchall()}
+
 		maxchan = max(score, key=score.get)
 		ratio = score[maxchan] / score[chan] if score[chan] > 0 else 0
-		if (ratio > 0.75 and maxchan != chan):
-			self.sayto(chan, '==> %s' % (maxchan,))
+		if (ratio > 2.0 and maxchan != chan):
+			self.sayto(chan, '%s: ==> %s' % (nick, maxchan))
 
 		print(msg)
 		for s in score.items():
@@ -66,14 +70,35 @@ class IRCCat(ircbot.SimpleIRCClient):
 		print('')
 
 
+
+def check_install(db):
+	"""Check whether table have been correctly set up."""
+	c = db.cursor()
+	c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Word'")
+	return c.fetchone() != None
+
+
+def install(db):
+	"""Initialize database"""
+	print('Installation...')
+	c = db.cursor()
+	c.execute('CREATE TABLE Word(word TEXT NOT NULL, chan TEXT COLLATE NOCASE NOT NULL, count INT NOT NULL DEFAULT 0, UNIQUE (word, chan))')
+	db.commit()
+
+
 def main():
 	server = "ulminfo.fr"
 	port = 6667
-	nickname = "nsfbot"
-	targets = words.keys()
+	nickname = "nsfbot24"
 
-	c = IRCCat(targets)
+	db = sqlite3.connect('nsfbot.db')
+
+	if not check_install(db):
+		install(db)
+	
+	c = IRCCat(chans, db)
 	try:
+		print('Connection...')
 		c.connect(server, port, nickname)
 	except ircbot.ServerConnectionError as x:
 		print(x)
